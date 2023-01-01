7230611c063a1abb7d69fc211ef67ef0f9f8c96a
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-01 14:16:37 +0600

    Update Main Js

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 
-
-console.log(window.location.href)
-
-
-const firsPage = () =>{
-    if(window.location.href.includes('home')){
+const firsPage = () => {
+    if (window.location.href.includes('home')) {
         window.location.href = '/index.html'
-    }else{
+    } else {
         window.location.href = '/home.html'
     }
-    
-}
 
-setTimeout(firsPage, 3000);
+}
+let timer;
+
+function startTimer() {
+    timer = setInterval(() => {
+        if (window.location.href.includes('home')) {
+            window.location.href = '/index.html'
+        } else {
+            window.location.href = '/home.html'
+        }
+    }, 1000);
+}
+startTimer();
+
+const timeBtn = document.getElementById('handleBtn');
+timeBtn.addEventListener('click', function () {
+    clearInterval(timer);
+    setTimeout(startTimer, 10000);
+})
+
+
